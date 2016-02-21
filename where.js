0d18498fe15d992e8b40779f0724c38ca2bf1bca
mostafa <mostafaahmed456@gmail.com>
2016-02-21 20:17:52 +0200

    first commit

---
+++ where.js
@@ -0,0 +1,20 @@
+myData = [
+[29.9668343,32.5498069, 'Suez, Suez, Suez Governorate, Egypt'],
+[30.0444196,31.2357116, 'Cairo, Cairo Governorate, Egypt'],
+[12.862807,30.217636, 'Sudan'],
+[42.340075,-71.0895367, 'Northeastern, Boston, MA 02115, USA'],
+[38.2113643,-85.7470011, 'Bradley Ave, Louisville, KY, USA'],
+[32.778949,35.019648, 'Technion/ Sports Building, Haifa'],
+[18.4574518,73.8837999, 'Vishwakarma Institutes Play Ground, Yashodhan Society, Kapil Nagar, Kondhwa Budrukh, Vishwakarma, Maharashtra 411048, India'],
+[42.4036847,-71.120482, 'South Hall Tufts University, 30 Lower Campus Rd, Somerville, MA 02144, USA'],
+[-37.914517,145.1303881, 'Monash College, Wellington Rd, Clayton VIC 3168, Australia'],
+[53.2948229,69.4047872, 'Kokshetau 020000, Kazakhstan'],
+[40.7127837,-74.0059413, 'New York, NY, USA'],
+[52.2869741,104.3050183, 'Irkutsk, Irkutsk Oblast, Russia'],
+[31.1790053,121.4237432, 'Shang Hai Jiao Tong Da Xue Fu Shu Di Liu Ren Min Yi Yuan, Xuhui Qu, Shanghai Shi, China, 200231'],
+[8.481302,4.611479, 'University Rd, Ilorin, Nigeria'],
+[-38.3105571,146.4292232, 'Monash University Gippsland Student Lounge, 7N Mary Grant Bruce Dr, Churchill VIC 3842, Australia'],
+[-34.9221963,138.5922272, 'Yungondi Building, North Terrace, Adelaide SA 5000, Australia'],
+[47.80949,13.05501, 'Salzburg, Austria'],
+[27.7518284,-82.6267345, 'St. Petersburg, FL, USA']
+];
